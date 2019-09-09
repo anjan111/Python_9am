991eef752ffc05efe151ab142e9f1e5aac93193f
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-09-08 21:19:11 -0700

    Add files via upload

---
+++ 001_python basic_syntax/002_fundametal.py
@@ -0,0 +1,30 @@
+# fundamental datatype
+print "************ int ************"
+var = 123
+print "var : ",var
+print type(var)
+print id(var)
+print "************ float ************"
+var = 12.233
+print "var : ",var
+print type(var)
+print id(var)
+print "************ complex ************"
+var = 12+3j
+print "var : ",var
+print type(var)
+print id(var)
+print "************ bool ************"
+var = True
+print "var : ",var
+print type(var)
+print id(var)
+var = False
+print "var : ",var
+print type(var)
+print id(var)
+print "************ None ************"
+var = None # no data
+print "var : ",var
+print type(var)
+print id(var)
---
+++ 001_python basic_syntax/003_sequencial.py
@@ -0,0 +1,26 @@
+# sequencial datatype
+print " ****************  str *************"
+var = "python Program"
+print "var : ",var
+print type(var)
+print id(var)
+print " ****************  list *************"
+var = [12, 56.45,4+6j]
+print "var : ",var
+print type(var)
+print id(var)
+print " ****************  tuple *************"
+var = (12, 56.45,4+6j)
+print "var : ",var
+print type(var)
+print id(var)
+print " ****************  set  *************"
+var = {12, 56.45,4+6j}
+print "var : ",var
+print type(var)
+print id(var)
+print " **************** dict  *************"
+var = {12:56.45,4+6j:"soc"}
+print "var : ",var
+print type(var)
+print id(var)
