5703be3c56da4684ef7d5299705b77f91dc782d8
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-09-10 21:28:04 -0700

    Add files via upload

---
+++ 002_python_built-in-function/005_number_system.py
@@ -0,0 +1,18 @@
+Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:37:19) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> var =31
+>>> bin(31)
+'0b11111'
+>>> oct(31)
+'037'
+>>> hex(31)
+'0x1f'
+>>> int('0b101010',2)
+42
+>>> int('0b10110',2)
+22
+>>> int('0127',8)
+87
+>>> int('0x1f',16)
+31
+>>> 
